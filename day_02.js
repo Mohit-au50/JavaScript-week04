8a77a0cb639f20ce7253ce469487f89e285f9f09
Mohit <109670799+Mohit-au50@users.noreply.github.com>
2023-08-09 20:22:45 +0530

    js codingChallenge day02

---
+++ day_02.js
@@ -0,0 +1,13 @@
+// Q-1) Write a function to sort an array of numbers.
+// Input: [8,5,6,3]
+// Output: [3,5,6,8]
+
+// input is given in the question
+let input = [8, 5, 6, 3];
+console.log("The input array is", input);
+
+// "sort()" function will sort the array and save the sorted array value in "newInp" variable
+let newInp = input.sort();
+
+// newInp will be printed in the console
+console.log("\nThe Output:", newInp);
